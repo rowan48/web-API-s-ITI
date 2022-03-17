e998a6627cad6469b01ab0bf5d6c3d20e353e771
gitlabtwo <rowan.rowanahmedali11@gmail.com>
2022-03-17 15:04:30 +0200

    web-api

---
+++ weather-api/index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>	Check Weather</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form action="action.php" method="post">
+		<select name="city" >
+			<option value="Cairo">cairo</option>
+			<option value="Alexandria">alex</option>
+			<option value="Fayoum">Fayoum</option>
+            <option value="El-Mahalla El-Kubra	">El-Mahalla El-Kubra	</option>
+            <option value="Tanta">Tanta</option>
+            <option value="Damanhur">Damanhur	</option>
+            <option value="al-Minya">al-Minya	</option>
+            <option value="Beni Suef">Beni Suef	</option>
+            <option value="Qena">Qena</option>
+            <option value="Sohag">Sohag</option>
+            <option value="Hurghada">Hurghada</option>
+            <option value="6th of October City">6th of October City</option>
+            <option value="Shibin El Kom">Shibin El Kom	</option>
+            <option value="Kafr el-Sheikh">Kafr el-Sheikh</option>
+            <option value="Arish">Arish</option>
+            <option value="Mallawi">Mallawi	</option>
+
+
+
+        </select>
+		<button type="submit" name="submit">Submit</button>
+	</form>
+</body>
+</html>
